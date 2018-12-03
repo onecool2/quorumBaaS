41af9a8a58ae8610dd6b2b1b2a4dd8423978460d
onecool2 <w_x_x_max@126.com>
2018-12-03 21:47:03 +0800

    complete basic run

--- quorum/geth.sh
+++ quorum/geth.sh
@@ -1,3 +1,23 @@
-mv /shareStorage/node${NODE_ID}/quorum ~/quorum
+#!/bin/bash
+echo Wait 25 seconds for tessera is ready
+i=500
+while [ $i -ge 0 ]
+do
+    if [ -w "/tesserasocket/tm.ipc" ];then
+        break
+    fi
+    sleep 5
+    let i-=1
+    echo $i
+done
+
+if [ $i == -1 ]; then
+   echo "Couldn't start quorum"
+   exit -1
+fi
+echo "Found tessera ipc socket, continue starting quorum"
+
+cp -r /shareStorage/node${NODE_ID}/quorum ~/quorum
 geth --datadir ~/quorum init /shareStorage/clique-genesis.json
-geth $LAUNCH_PARAMETERS PRIVATE_CONFIG=$DDIR/tm.ipc --datadir ~/node${NODE_ID} --networkid ${NETWORK_ID} --rpcport ${RPC_PORT} --port ${P2P_PORT} 
+touch ~/quorum/password
+PRIVATE_CONFIG=/tesserasocket/tm.ipc geth $LAUNCH_GETH_PARAMETERS --password ~/quorum/password --unlock 0xed9d02e382b34818e88b88a309c7fe71e65f419d --etherbase 0xed9d02e382b34818e88b88a309c7fe71e65f419d --datadir ~/quorum --networkid ${NETWORK_ID} --rpcport ${RPC_PORT} --port ${P2P_PORT} 
--- tessera/tessera.sh
+++ tessera/tessera.sh
@@ -1,6 +1,5 @@
-mv /shareStorage/node${NODE_ID}/tessera ~/tessera
+cp -r /shareStorage/node${NODE_ID}/tessera ~/tessera
+echo "127.0.0.1 ${TESSERASERVICENAME}" >> /etc/hosts
 sed -i 's/TESSERA_PORT/'${TESSERA_PORT}'/g' ~/tessera/tessera-config.json
-sed -i 's/TESSERASERVCIENAME/'${TESSERASERVCIENAME}'/g' ~/tessera/tessera-config.json
-sed -i 's/DDIR/'${DDIR}'/g' ~/tessera/tessera-config.json
-sed -i 's/TESSERADIR/'${TESSERADIR}'/g' ~/tessera/tessera-config.json
+sed -i 's/TESSERASERVICENAME/'${TESSERASERVICENAME}'/g' ~/tessera/tessera-config.json
 java $DEBUG $MEMORY $LAUNCH_TESSERA_PARAMETERS -configfile ~/tessera/tessera-config.json
